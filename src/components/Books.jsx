2b3ab792f0c701ecd1f12446ffc904abf2814d62
Ryan <ryfly2016@gmail.com>
2025-04-11 16:33:32 -0600

    add 'available' or 'unavailable' badges to each card

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -7,14 +7,23 @@ export default function Books() {
     <div className="m-5">
       <div className="row row-cols-4">
         {books?.map((obj) => {
+          let available = "";
+          if (obj.available === true) {
+            available = "Available";
+          } else {
+            available = "Unavailable";
+          }
           return (
-            <div className="card mb-1 text-bg-light" key={obj.id} id="card">
+            <div className="card mb-3 text-bg-light" key={obj.id} id="card">
               <img
                 src={obj.coverimage}
                 className="card-img-top"
                 alt={`The cover of ${obj.title}`}
               />
               <div className="card-body">
+                <span class="position-absolute top-0 start-50 translate-middle badge rounded-pill bg-primary">
+                  {available}
+                </span>
                 <h5 className="card-title">{obj.title}</h5>
                 <p className="card-text">{obj.description}</p>
                 <p className="card-text">
