28868d5894e3bcf9bb9582ba580f7074495babd1
Ryan <ryfly2016@gmail.com>
2025-04-11 09:13:49 -0600

    test conflict

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ import { Router, Routes } from "react-router-dom";
 function App() {
   const [token, setToken] = useState(null);
 
-  return <></>;
+  return (
+    <>
+      <h1>Something</h1>
+    </>
+  );
 }
 
 export default App;
