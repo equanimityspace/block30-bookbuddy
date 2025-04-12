bad74e1304d7f420dc55b3fcced690e06382cc1b
DeadlySage <adrian.hartanto159@gmail.com>
2025-04-11 17:15:39 -0700

    working login and register pages, implemented local storage for register and login api calls

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
+import { registerReducer, loginReducer } from "../app/librarySlice";
 import api from "../api/api";
 
 const store = configureStore({
   reducer: {
     [api.reducerPath]: api.reducer,
+    register: registerReducer,
+    login: loginReducer,
   },
   middleware: (getDefaultMiddleWare) =>
     getDefaultMiddleWare().concat(api.middleware),
