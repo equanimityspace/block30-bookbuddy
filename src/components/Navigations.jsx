ab7a0f895627e72d8980077e9cfff0a5691f1717
Alyssa <ashinnickmitchell@gmail.com>
2025-04-11 23:39:16 -0600

    styled search bar and mini nav bar

--- src/components/Navigations.jsx
+++ src/components/Navigations.jsx
@@ -1,17 +1,28 @@
-import { NavLink } from "react-router-dom";
+import "../index.css";
+import { NavLink, useLocation } from "react-router-dom";
 
 export default function Navigations() {
+  const location = useLocation();
+  const titles = {
+    "/": "Welcome!",
+    "/singleBook": "Book Details",
+    "/login": "Welcome!",
+    "/register": "Create an Account",
+  };
+
+  const pageTitle = titles[location.pathname];
+
   return (
     <div>
-      <nav className="navbar navbar-light bg-light justify-content-between">
+      <nav className="navbar">
         <NavLink className="navbar-brand" to="/">
-          Books Home
+          Library Name
         </NavLink>
-        <form className="form-inline">
+        <form className="searchbar">
           <input
             className="form-control mr-sm-2"
             type="search"
-            placeholder="Search"
+            placeholder="Search all books..."
             aria-label="Search"
           />
           <button
@@ -21,22 +32,31 @@ export default function Navigations() {
             Search
           </button>
         </form>
-      <nav className:"mininav">
-        <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-        </li>
       </nav>
-    </nav>
- </div>
+      <nav className="mininav">
+        <h1> {pageTitle} </h1>
+        <div className="mininav dropdown">
+          <NavLink
+            className="mininav-link dropdown-toggle"
+            href="#"
+            id="mininavDropdown"
+            role="button"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            My Account
+          </NavLink>
+          <div className="dropdown-menu" aria-labelledby="mininavDropdown">
+            <NavLink className="mininav-item" to="/mybooks">
+              My Books
+            </NavLink>
+          </div>
+        </div>
+      </nav>
+    </div>
   );
 }
 
 /* TODO - add your code to create a functional React component that renders a navigation bar for the different views in your single page application. You may consider conditionally rendering some options - for example 'Login' should be available if someone has not logged in yet. */
+// navbar-light bg-light justify-content-between
