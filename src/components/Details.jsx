8ac17780c3e1c7c01cebe53bd609432a790f757c
Ryan <ryfly2016@gmail.com>
2025-04-12 21:20:38 -0600

    add buttons and some simple styling

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -2,6 +2,7 @@ import Carousel from "react-bootstrap/Carousel";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 
 import {
   useGetBookDetailsQuery,
@@ -20,43 +21,58 @@ export default function SingleBook({ book, setBook }) {
   }
 
   return (
-    <Container>
-      <Row className="justify-content-md-center">
-        <Carousel className="m-3">
-          <Carousel.Item>
-            <img
-              src={bookDetail?.coverimage}
-              className="carousel-image-fill"
-              text="First slide"
-            />
-            <Carousel.Caption>
-              <h3>{bookDetail?.title}</h3>
-              <p>{bookDetail?.author}</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              src={bookDetail?.coverimage}
-              className="carousel-image-fill"
-              text="Second slide"
-            />
-            <Carousel.Caption>
-              <h3>Description</h3>
-              <p>{bookDetail?.description}</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              src={bookDetail?.coverimage}
-              className="carousel-image-fill"
-              text="Third slide"
-            />
-            <Carousel.Caption>
-              <h3>{available}</h3>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
-      </Row>
-    </Container>
+    <>
+      <Container>
+        <Row>
+          <Carousel className="m-3">
+            <Carousel.Item>
+              <img
+                src={bookDetail?.coverimage}
+                className="carousel-image-fill"
+                text="First slide"
+              />
+              <Carousel.Caption>
+                <h3>{bookDetail?.title}</h3>
+                <p>{bookDetail?.author}</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                src={bookDetail?.coverimage}
+                className="carousel-image-fill"
+                text="Second slide"
+              />
+              <Carousel.Caption>
+                <h3>Description</h3>
+                <p>{bookDetail?.description}</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                src={bookDetail?.coverimage}
+                className="carousel-image-fill"
+                text="Third slide"
+              />
+              <Carousel.Caption>
+                <h3>{available}</h3>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+        </Row>
+      </Container>
+      <Container className="d-flex justify-content-center mb-3">
+        <Row className="w-50">
+          <Col className="text-center">
+            <Button variant="success">Check In</Button>
+          </Col>
+          <Col className="text-center">
+            <Button variant="danger">Check Out</Button>
+          </Col>
+          <Col className="text-center">
+            <Button variant="primary">Home</Button>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
