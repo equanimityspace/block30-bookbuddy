7af2884b9c70d825f37ebdac3c74977fa00435e3
Ryan <ryfly2016@gmail.com>
2025-04-15 07:08:00 -0600

    switch between button to login or go to account if token exists

--- src/components/Navigations.jsx
+++ src/components/Navigations.jsx
@@ -1,10 +1,14 @@
 import { useState, useEffect } from "react";
 import { NavLink, useLocation } from "react-router-dom";
+import { getToken } from "../app/tokenService";
 import "../index.css";
 
 import { useGetAllBooksQuery } from "../app/librarySlice";
 
 export default function Navigations({ setFilteredBooks }) {
+  // get token if it exists
+  const token = getToken();
+
   const location = useLocation();
   const titles = {
     "/": "Welcome!",
@@ -64,26 +68,28 @@ export default function Navigations({ setFilteredBooks }) {
       </nav>
       <nav className="mininav">
         <h1> {pageTitle} </h1>
-        <NavLink to="/login"> Login </NavLink>
-        <NavLink to="/register"> Sign-up </NavLink>
-        <div className="mininav dropdown">
-          <NavLink
-            className="mininav-link dropdown-toggle"
-            to="/account"
-            id="mininavDropdown"
-            role="button"
-            data-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="false"
-          >
-            My Account
-          </NavLink>
-          <div className="dropdown-menu" aria-labelledby="mininavDropdown">
-            <NavLink className="mininav-item" to="/mybooks">
-              My Books
+        {token === null ? (
+          <NavLink to="/login"> Login / Sign Up </NavLink>
+        ) : (
+          <div className="mininav dropdown">
+            <NavLink
+              className="mininav-link dropdown-toggle"
+              to="/account"
+              id="mininavDropdown"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              My Account
             </NavLink>
+            <div className="dropdown-menu" aria-labelledby="mininavDropdown">
+              <NavLink className="mininav-item" to="/mybooks">
+                My Books
+              </NavLink>
+            </div>
           </div>
-        </div>
+        )}
       </nav>
     </div>
   );
